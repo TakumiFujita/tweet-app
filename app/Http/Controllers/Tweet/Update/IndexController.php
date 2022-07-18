cb12eeba307a891d42e58514a702343d1bfb3c8e
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2022-07-18 09:35:34 +0900

    つぶやきの編集処理を作成

---
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Tweet\Update;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $tweetId = (int) $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+
+        //上記でfirstOrFailを使うことで以下のコードを省略できる
+        // if (is_null($tweet)) {
+        //     throw new NotFoundHttpException('存在しないつぶやきです');
+        // }
+        return view('tweet.update')->with('tweet', $tweet);
+    }
+}
